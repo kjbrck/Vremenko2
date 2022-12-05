520023d1879be5bbc8a3a6fb9b1f3a8b07e39c9b
Your Name <you@example.com>
2022-12-05 19:48:43 +0100

    popravek WeatherStation razreda

--- web/Models/WeatherStation.cs
+++ web/Models/WeatherStation.cs
@@ -2,13 +2,9 @@ namespace web.Models
 {
     public class WeatherStation
     {
-        public string? MeteoId { get; set; }
+        public string MeteoId { get; set; }
         public DateTime? Time { get; set; }
         public float? Temp { get; set; }
         public float? Hum { get; set; }
-
-        public WeatherStation(){
-            MeteoId = null;
-        }
     }
 }
