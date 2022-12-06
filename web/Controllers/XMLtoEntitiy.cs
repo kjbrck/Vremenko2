a09327757f025d0dd2a87f17ea4c894bc978a965
mr6231 <117918828+mr6231@users.noreply.github.com>
2022-12-06 13:20:03 +0100

    kk

--- web/Controllers/XMLtoEntitiy.cs
+++ web/Controllers/XMLtoEntitiy.cs
@@ -102,6 +102,7 @@ public class XMLtoEntity : Controller
                         cn.Close();
                         
                     }
+                    //Console.WriteLine(zvp.meteoID+" "+isZapisVMeritvi);
                     if(isZapisVMeritvi==0)
                     {
                         /*
