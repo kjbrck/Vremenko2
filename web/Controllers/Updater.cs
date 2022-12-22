f19ef51acc8b6cab6e0e3f6213a0e0c3099efad3
mr6231 <117918828+mr6231@users.noreply.github.com>
2022-12-22 11:59:00 +0100

    modified

--- web/Controllers/Updater.cs
+++ web/Controllers/Updater.cs
@@ -15,20 +15,20 @@ public class Updater : Controller
     public static void UpdateIzbira(string userID, string[] meteosiID)
     {
         try{
-        using(SqlConnection cn=new SqlConnection("Server=uni-db.database.windows.net;Database=University;User Id=university-sa;Password=yourStrong(!)Password;"))
-        {
-            foreach(string x in meteosiID){
+            using(SqlConnection cn=new SqlConnection("Server=uni-db.database.windows.net;Database=University;User Id=university-sa;Password=yourStrong(!)Password;"))
+            {
+                foreach(string x in meteosiID){
 
-                string query = "INSERT INTO [dbo].[izbor] VALUES('"+x+"','"+userID+"');";
+                    string query = "INSERT INTO [dbo].[izbor] VALUES('"+x+"','"+userID+"');";
 
-                using(SqlCommand command = new SqlCommand(query, cn))
-                {
-                    cn.Open();
-                    command.ExecuteNonQuery();
-                    cn.Close();
+                    using(SqlCommand command = new SqlCommand(query, cn))
+                    {
+                        cn.Open();
+                        command.ExecuteNonQuery();
+                        cn.Close();
+                    }
                 }
             }
-        }
         }
         catch(Exception e){
             Console.WriteLine(e.StackTrace);
