c0fd2823286599a19e48a90c98cc409f2d0e9908
mr6231 <117918828+mr6231@users.noreply.github.com>
2022-12-22 11:57:12 +0100

    updater za user izbira

---
+++ web/Controllers/Updater.cs
@@ -0,0 +1,37 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using web.Models;
+using System.Xml;
+using System.Xml.XPath;
+
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+using System.Data;
+namespace web.Controllers;
+
+public class Updater : Controller
+{
+    public static void UpdateIzbira(string userID, string[] meteosiID)
+    {
+        try{
+        using(SqlConnection cn=new SqlConnection("Server=uni-db.database.windows.net;Database=University;User Id=university-sa;Password=yourStrong(!)Password;"))
+        {
+            foreach(string x in meteosiID){
+
+                string query = "INSERT INTO [dbo].[izbor] VALUES('"+x+"','"+userID+"');";
+
+                using(SqlCommand command = new SqlCommand(query, cn))
+                {
+                    cn.Open();
+                    command.ExecuteNonQuery();
+                    cn.Close();
+                }
+            }
+        }
+        }
+        catch(Exception e){
+            Console.WriteLine(e.StackTrace);
+        }
+    }
+}
